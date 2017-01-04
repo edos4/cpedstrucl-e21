f8cba50102ef88fd8067ef7b858b79e272c2eadf
Edwin Richbald A. Salinas <edsalinas7b@yahoo.com>
2017-01-05 07:13:42 +0800

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,32 +1,43 @@
 #include<stdio.h> 
 #include<stdlib.h>
 
-  struct node {
-    int data;
-    struct node * next;
-  } *head;
+struct node {
+	int data;
+	struct node * next;
+} *head;
+  
+int count() {
+  struct node * n;
+  int c = 0;
+  n = head;
+  while (n != NULL) {
+    n = n -> next;
+    c++;
+  }
+  return c;
+}
 
 void append(int num) {
   struct node * temp, * right;
   temp = (struct node * ) malloc(sizeof(struct node));
-  temp - > data = num;
+  temp -> data = num;
   right = (struct node * ) head;
-  while (right - > next != NULL)
-    right = right - > next;
-  right - > next = temp;
+  while (right -> next != NULL)
+    right = right -> next;
+  right -> next = temp;
   right = temp;
-  right - > next = NULL;
+  right -> next = NULL;
 }
 
 void add(int num) {
   struct node * temp;
   temp = (struct node * ) malloc(sizeof(struct node));
-  temp - > data = num;
+  temp -> data = num;
   if (head == NULL) {
     head = temp;
-    head - > next = NULL;
+    head -> next = NULL;
   } else {
-    temp - > next = head;
+    temp -> next = head;
     head = temp;
   }
 }
@@ -37,14 +48,36 @@ void addafter(int num, int loc) {
   right = head;
   for (i = 1; i < loc; i++) {
     left = right;
-    right = right - > next;
+    right = right -> next;
   }
   temp = (struct node * ) malloc(sizeof(struct node));
-  temp - > data = num;
-  left - > next = temp;
+  temp -> data = num;
+  left -> next = temp;
   left = temp;
-  left - > next = right;
+  left -> next = right;
   return;
+}
+
+int ddelete(int num) {
+  struct node * temp, * prev;
+  temp = head;
+  while (temp != NULL) {
+    if (temp -> data == num) {
+      if (temp == head) {
+        head = temp -> next;
+        free(temp);
+        return 1;
+      } else {
+        prev -> next = temp -> next;
+        free(temp);
+        return 1;
+      }
+    } else {
+      prev = temp;
+      temp = temp -> next;
+    }
+  }
+  return 0;
 }
 
 void insert(int num) {
@@ -55,9 +88,9 @@ void insert(int num) {
     add(num);
   } else {
     while (temp != NULL) {
-      if (temp - > data < num)
+      if (temp -> data < num)
         c++;
-      temp = temp - > next;
+      temp = temp -> next;
     }
     if (c == 0)
       add(num);
@@ -68,27 +101,7 @@ void insert(int num) {
   }
 }
 
-int delete(int num) {
-  struct node * temp, * prev;
-  temp = head;
-  while (temp != NULL) {
-    if (temp - > data == num) {
-      if (temp == head) {
-        head = temp - > next;
-        free(temp);
-        return 1;
-      } else {
-        prev - > next = temp - > next;
-        free(temp);
-        return 1;
-      }
-    } else {
-      prev = temp;
-      temp = temp - > next;
-    }
-  }
-  return 0;
-}
+
 
 void display(struct node * r) {
   r = head;
@@ -96,21 +109,10 @@ void display(struct node * r) {
     return;
   }
   while (r != NULL) {
-    printf("%d ", r - > data);
-    r = r - > next;
+    printf("%d ", r -> data);
+    r = r -> next;
   }
   printf("\n");
-}
-
-int count() {
-  struct node * n;
-  int c = 0;
-  n = head;
-  while (n != NULL) {
-    n = n - > next;
-    c++;
-  }
-  return c;
 }
 
 int main() {
@@ -153,7 +155,7 @@ int main() {
         else {
           printf("Enter the number to delete : ");
           scanf("%d", & num);
-          if (delete(num))
+          if (ddelete(num))
             printf("%d deleted successfully\n", num);
           else
             printf("%d not found in the list\n", num);
